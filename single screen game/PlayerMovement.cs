a7ee469ba41c80d7b24414cd8a434055a091c3ad
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2021-03-05 14:36:04 +0000

    created

---
+++ single screen game/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+
+    private float moveDir;
+    private bool canJump;
+    private Rigidbody2D myRB;
+
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveAxis = Vector3.right * moveDir;
+        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+        {
+            myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        print(context.ReadValue<float>());
+        moveDir = context.ReadValue<float>();
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (canJump)
+        {
+            print(context.ReadValue<bool>());
+            myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+}
