b76f8c01d551ca0e0be9725f1b60e1380dcafd8d
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2021-03-17 18:13:42 +0000

    added collectable key and made slide fell better

--- single screen game/Assets/Scripts/GameManager.cs
+++ single screen game/Assets/Scripts/GameManager.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private UnityEvent<string> addScore;
     private Vector3 startPos;
+  private int score;
 
      private void Start()
         {
             startPos = player.transform.position;
                Time.timeScale = 0;
-
+              score = 0;
             }
 
              public void RespawnPlayer()
@@ -28,4 +30,15 @@ public class GameManager : MonoBehaviour
                 {
                  Time.timeScale = 1;
                  }
+
+                  public void AddScore(int scoreAmt)
+                     {
+                         score += scoreAmt;
+                         UpdateUI();
+                     }
+
+                     private void UpdateUI()
+                         {
+                             addScore.Invoke(score.ToString());
+                         }
 }
